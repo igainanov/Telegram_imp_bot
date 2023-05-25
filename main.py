ba21bfdee94a035d7c55e1adf6b79bca4c09f994
igainanov <128050681+igainanov@users.noreply.github.com>
2023-05-25 22:46:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from imp_bot import run_imp_bot
 
 if __name__ == '__main__':
     # the constant Token got from BotFather
-    token = "6195855561:AAFBDOaZc4CD4FqfJxVvhKOukCj22XdO700"
+    token = ""
 
     # start the bot of stock market indexes, Moon's and Solar system planets' data
     run_imp_bot(token)
